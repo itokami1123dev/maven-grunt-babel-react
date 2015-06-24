d03ea9685245c322679fd23bac1e99798662d456
itokami1123dev <kamiari819dev@gmail.com>
2015-06-24 12:42:35 +0900

    update OrderItem

--- src/main/resources/static/js/src/components/OrderItem.jsx
+++ src/main/resources/static/js/src/components/OrderItem.jsx
@@ -8,28 +8,60 @@ export default class OrderItem extends React.Component {
         };
     }
 
-    onClick(event) {
-        console.log("event=", event);
-        //this.props.onClick(event);
+    onClearClick(/*SyntheticMouseEvent*/ event) {
+        //console.log("event=", event);
+        this.setState({
+            unitPrice: 0,
+            quantity: 0
+        });
     }
 
     changeUnitPrice(/*SyntheticEvent*/ event) {
-        var unitPrice = !event.target.value ? 0 : event.target.value - 0;
+        var unitPrice = event.target.value - 0;
+        unitPrice = isNaN(unitPrice) ? 0 : unitPrice;
         this.setState({unitPrice: unitPrice});
     }
 
     changeQuantity(/*SyntheticEvent*/ event) {
-        var quantity = !event.target.value ? 0 : event.target.value - 0;
+        var quantity = event.target.value - 0;
+        quantity = isNaN(quantity) ? 0 : quantity;
         this.setState({quantity: quantity});
+    }
+
+    cancelSubmit(/*SyntheticEvent*/ event) {
+        //console.log("#submit event=", event);
+        event.preventDefault();
     }
 
     render() {
         return (
-            <div>
-                <input type="text" value={this.state.unitPrice} onChange={this.changeUnitPrice.bind(this)}/>
-                <input type="text" value={this.state.quantity} onChange={this.changeQuantity.bind(this)}/>
-                <button onClick={this.onClick.bind(this)}>delete</button>
-            </div>
+            <form className="form-inline" onSubmit={this.cancelSubmit}>
+                <input
+                    type="text"
+                    value={this.state.unitPrice}
+                    onChange={this.changeUnitPrice.bind(this)}
+                    className="form-control"
+                    />
+
+                <p className="form-control-static"> x </p>
+
+                <input
+                    type="text"
+                    value={this.state.quantity}
+                    onChange={this.changeQuantity.bind(this)}
+                    className="form-control"
+                    />
+
+                <p className="form-control-static"> = </p>
+
+                <p className="form-control-static">
+                    {this.state.unitPrice * this.state.quantity}
+                </p>
+
+                <button onClick={this.onClearClick.bind(this)}>
+                    CLEAR!
+                </button>
+            </form>
         );
     }
 }
